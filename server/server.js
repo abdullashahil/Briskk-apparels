cc176f337c7726f4bd735bb856351ea7cd719cdf
Abdulla Shahil <abdullashahil0077@gmail.com>
2025-03-24 00:29:00 +0530

    Project setup

---
+++ server/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import productRoutes from "./routes/productRoutes.js";
+
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.error("MongoDB Connection Error:", err));
+
+// Routes
+app.use("/api/products", productRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
