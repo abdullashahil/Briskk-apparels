1a7cf448e1d33aecca6ac9c861c230d53ed49f8d
Abdulla Shahil <abdullashahil0077@gmail.com>
2025-03-26 12:01:28 +0530

    Production ready

--- server/server.js
+++ server/server.js
@@ -12,9 +12,13 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.error("MongoDB Connection Error:", err));
+
 
 // Routes
 app.use("/api/products", productRoutes);
